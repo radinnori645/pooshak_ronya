7b842c34b4ea92919de7319c8fc7879d767940e1
radinnori645 <radinnori645@gmail.com>
2025-10-31 20:03:06 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
+#!/usr/bin/env bash
 set -o errexit
+
+# نصب پکیج‌ها از requirements.txt
 pip install -r requirements.txt
+
+# اجرای مهاجرت‌های دیتابیس
 python manage.py migrate
-python manage.py createsuperuser --noinput
+
+# ساخت سوپر یوزر بدون ورودی (اطلاعات باید از طریق env تعریف شده باشه)
+python manage.py createsuperuser --noinput || echo "Superuser already exists or environment variables missing."
+
+# جمع‌آوری فایل‌های استاتیک
 python manage.py collectstatic --no-input
